b2bb5cf66068e182102c92e40cf6612c6a661e49
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-03-14 16:14:10 +0530

    day06 complete

---
+++ day06A.py
@@ -0,0 +1,24 @@
+file = open('day06_input', 'r')
+
+s = []
+i = 1
+
+while True:
+    if len(s) == 4:
+        break
+
+    char = file.read(1)
+    if not char:
+        break
+
+    if char not in s:
+        s.append(char)
+    else:
+        s = s[s.index(char)+1:]
+        s.append(char)
+
+    i += 1
+
+file.close()
+
+print(i-1)
